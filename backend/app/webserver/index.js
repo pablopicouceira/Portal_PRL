fc93232264f28fe1c3968f67a9a947f4c868ecb6
pablopicouceira <pablo.pico.uceira@gmail.com>
2020-01-25 02:56:45 +0100

    folder structure created

---
+++ backend/app/webserver/index.js
@@ -0,0 +1,52 @@
+"use strict";
+
+const express = require("express");
+/*
+const accountRouter = require('./routes/account-router');
+const authRouter = require('./routes/auth-router');
+*/
+// const {
+//   accountRouter,
+//   authRouter,
+//   noteRouter,
+//   tagRouter,
+//   userRouter
+// } = require("./routes");
+
+const app = express();
+
+// app.use(express.json());
+// app.use("/api", accountRouter);
+// app.use("/api", authRouter);
+// app.use("/api", noteRouter);
+// app.use("/api", tagRouter);
+// app.use("/api", userRouter);
+
+let server = null;
+async function listen(port) {
+  if (server) {
+    return server;
+  }
+
+  try {
+    server = await app.listen(port);
+    return server;
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+}
+
+async function close() {
+  if (server) {
+    await server.close();
+    server = null;
+  } else {
+    console.error("Can not close a non started server");
+  }
+}
+
+module.exports = {
+  listen,
+  close
+};
