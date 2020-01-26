eef1b9488bedcf46a0805ed3e60573b23b89adfb
pablopicouceira <pablo.pico.uceira@gmail.com>
2020-01-26 19:21:11 +0100

    added delete-worker endpoint

--- backend/app/webserver/routes/worker-router.js
+++ backend/app/webserver/routes/worker-router.js
@@ -5,6 +5,7 @@ const express = require("express");
 const checkAccountSession = require("../controllers/account/check-account-session");
 
 const createWorker = require("../controllers/worker/create-worker-controller");
+const deleteWorker = require("../controllers/worker/delete-worker-controller");
 const getWorkers = require("../controllers/worker/get-workers-controller");
 const updateWorker = require("../controllers/worker/update-worker-controller");
 
@@ -13,5 +14,6 @@ const router = express.Router();
 router.post("/workers", checkAccountSession, createWorker);
 router.get("/workers", checkAccountSession, getWorkers);
 router.put("/workers/:workerId", checkAccountSession, updateWorker);
+router.delete("/workers/:workerId", checkAccountSession, deleteWorker);
 
 module.exports = router;
