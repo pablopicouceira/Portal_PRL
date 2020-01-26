eb8d5a16d48ba1468ce9eb89384dc82196828370
pablopicouceira <pablo.pico.uceira@gmail.com>
2020-01-26 22:02:57 +0100

    added get-inactive-projects endpoint

---
+++ backend/app/webserver/controllers/project/get-inactive-projects-controller.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const mysqlPool = require("../../../database/mysql-pool");
+
+async function getProjects(req, res, next) {
+  const { userId } = req.claims;
+
+  // 2. Select all tags
+  try {
+    const connection = await mysqlPool.getConnection();
+    const sqlQuery = `SELECT *
+      FROM Actuaciones
+      WHERE deleted_At IS NOT NULL`;
+    const [rows] = await connection.execute(sqlQuery);
+    connection.release();
+
+    // preparar respuesta
+    const projects = rows.map(project => {
+      return {
+        ...project,
+        created_At: undefined,
+        updated_At: undefined,
+        deleted_At: undefined
+        // createdAt: tag.created_at,
+        // updatedAt: tag.updated_at,
+        // user_id: undefined,
+        // created_at: undefined,
+        // updated_at: undefined
+      };
+    });
+
+    return res.status(200).send(projects);
+  } catch (e) {
+    console.error(e);
+    return res.status(500).send();
+  }
+}
+
+module.exports = getProjects;
