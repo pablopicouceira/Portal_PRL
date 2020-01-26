7167f706e304deb09a243c8df00c8725733c8c95
pablopicouceira <pablo.pico.uceira@gmail.com>
2020-01-26 22:27:50 +0100

    added get-inactive-workers endpoint

---
+++ backend/app/webserver/controllers/worker/get-inactive-workers-controller.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const mysqlPool = require("../../../database/mysql-pool");
+
+async function getInactiveWorkers(req, res, next) {
+  const { userId } = req.claims;
+
+  // 2. Select all tags
+  try {
+    const connection = await mysqlPool.getConnection();
+    const sqlQuery = `SELECT *
+      FROM Trabajadores
+      WHERE deleted_At IS NOT NULL`;
+    //WHERE user_id = ?`;
+    const [rows] = await connection.execute(sqlQuery); //, [userId]);
+    connection.release();
+    console.log(await connection.execute(sqlQuery));
+    // preparar respuesta
+    const Trabajadores = rows.map(trabajador => {
+      return {
+        ...trabajador,
+        created_At: undefined,
+        updated_At: undefined,
+        deleted_At: undefined
+      };
+    });
+
+    return res.status(200).send(Trabajadores);
+  } catch (e) {
+    console.error(e);
+    return res.status(500).send();
+  }
+}
+
+module.exports = getInactiveWorkers;
--- backend/app/webserver/routes/worker-router.js
+++ backend/app/webserver/routes/worker-router.js
@@ -6,6 +6,7 @@ const checkAccountSession = require("../controllers/account/check-account-sessio
 
 const createWorker = require("../controllers/worker/create-worker-controller");
 const deleteWorker = require("../controllers/worker/delete-worker-controller");
+const getInactiveWorkers = require("../controllers/worker/get-inactive-workers-controller");
 const getWorkers = require("../controllers/worker/get-workers-controller");
 const updateWorker = require("../controllers/worker/update-worker-controller");
 
@@ -13,6 +14,7 @@ const router = express.Router();
 
 router.post("/workers", checkAccountSession, createWorker);
 router.get("/workers", checkAccountSession, getWorkers);
+router.get("/workers/inactive", checkAccountSession, getInactiveWorkers);
 router.put("/workers/:workerId", checkAccountSession, updateWorker);
 router.delete("/workers/:workerId", checkAccountSession, deleteWorker);
 
