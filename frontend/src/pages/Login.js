8c284de65c0f4f4a8c35f7baff12c218b8ef3e9a
pablopicouceira <pablo.pico.uceira@gmail.com>
2020-02-03 00:14:47 +0100

    login modified

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,25 +1,44 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import useForm from "react-hook-form";
+import { useForm, ErrorMessage } from "react-hook-form";
 
 export function Login() {
+  const { handleSubmit, register, errors } = useForm();
+  const login = data => console.log(data);
   return (
     <React.Fragment>
       <main className="centered-container">
         <h3>Please Login</h3>
-        <form>
+        <form onSubmit={handleSubmit(login)}>
           <div className="form-control">
             <label>Email</label>
-            <input type="email" placeholder="Please enter your email"></input>
-            <span className="errorMessage"></span>
+            <input
+              ref={register({
+                required: true,
+                pattern: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+              })}
+              name="email"
+              type="email"
+              placeholder="Please enter your email"
+            ></input>
+            {errors.email && (
+              <span className="errorMessage">The email is not valid</span>
+            )}
           </div>
           <div className="form-control">
             <label>Password</label>
             <input
+              ref={register({
+                required: true,
+                minLength: 6
+              })}
+              name="password"
               type="password"
               placeholder="Please enter your password"
             ></input>
-            <span className="errorMessage"></span>
+            {errors.password && (
+              <span className="errorMessage">The password is not valid</span>
+            )}
           </div>
           <div className="button-container">
             <button type="submit" className="btn">
