80d3f02fe430c8de321646cc0b3f003af463ae6a
pablopicouceira <pablo.pico.uceira@gmail.com>
2020-02-05 02:34:29 +0100

    get workers frontend operative

--- frontend/src/pages/Trabajadores.js
+++ frontend/src/pages/Trabajadores.js
@@ -1,18 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { getWorkers } from "../http/workersService";
 import { useAuth } from "../context/auth-context";
 
 export function Trabajadores() {
   const { currentUser } = useAuth();
+  const [workers, setWorkers] = useState([]);
 
   useEffect(() => {
-    getWorkers(currentUser.accessToken);
-  });
+    getWorkers(currentUser.accessToken).then(response => {
+      setWorkers(response.data);
+    });
+  }, []);
 
   return (
     <div className="App">
       <button>Ver Trabajadores</button>
-      <p>Trabajadores</p>
+      <h1>Trabajadores</h1>
+      <ul>
+        {workers.map(worker => (
+          <li key={worker.id}>
+            {worker.apellidos}, {worker.nombre} {worker.dni}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
