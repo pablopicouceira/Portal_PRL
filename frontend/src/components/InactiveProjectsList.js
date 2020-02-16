6babeb7feea181549b4168f40b62f2246243d680
pablopicouceira <pablo.pico.uceira@gmail.com>
2020-02-16 01:27:13 +0100

    Modifications in components

---
+++ frontend/src/components/InactiveProjectsList.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "../css/ProjectsList.css";
+
+export function InactiveProjectsList({
+  projects,
+  selectedIndex,
+  onProjectSelected
+}) {
+  return (
+    <React.Fragment>
+      <div className="App">
+        <h1>Actuaciones</h1>
+        <ul className="li">
+          {projects.map((project, index) => (
+            <li
+              key={project.id}
+              onClick={() => onProjectSelected(index)}
+              className={selectedIndex === index && "ProjectsList-selected"}
+            >
+              {project.nombre}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </React.Fragment>
+  );
+}
